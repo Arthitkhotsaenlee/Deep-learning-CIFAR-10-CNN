b6fff157dc0d6124baecfd31339faf4adb79cf16
arthitkhotsaenlee <arthit@mindampltd.com>
2022-07-27 10:29:25 +0800

    Load dataset and create model

--- CNN with cifar10.py
+++ CNN with cifar10.py
@@ -2,14 +2,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from keras.datasets import cifar10
+import sys
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense, Activation, Permute, Dropout
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, AveragePooling2D
-from tensorflow.keras.layers import SeparableConv2D, DepthwiseConv2D
-from tensorflow.keras.layers import BatchNormalization
-from tensorflow.keras.layers import SpatialDropout2D
 from tensorflow.keras.layers import Input, Flatten
-from tensorflow.keras.constraints import max_norm
+
 
 
 def load_dataset():
@@ -34,10 +32,70 @@ def one_hot(y):
     return one_hot_y
 
 def CNN_model(input_shape=(32,32,3)):
-    input = input_shape
+    input1 = Input(shape=input_shape)
+    ## block 1 ##
+    block1 = Conv2D(32,(3,3),activation="relu", kernel_initializer="he_uniform", padding="same",input_shape=input_shape)(input1)
+    block1 = Conv2D(32,(3,3),activation="relu", kernel_initializer="he_uniform", padding="same",input_shape=input_shape)(block1)
+    block1 = MaxPooling2D(2,2)(block1)
+    block1 = Dropout(0.2)(block1)
+    ### block2 ###
+    block2 = Conv2D(64,(3,3),activation="relu", kernel_initializer="he_uniform", padding="same",input_shape=input_shape)(block1)
+    block2 = Conv2D(64,(3,3),activation="relu", kernel_initializer="he_uniform", padding="same",input_shape=input_shape)(block2)
+    block2 = MaxPooling2D(2, 2)(block2)
+    block2 = Dropout(0.2)(block2)
+    ## block3 ##
+    block3 = Conv2D(128,(3,3),activation="relu", kernel_initializer="he_uniform", padding="same",input_shape=input_shape)(block2)
+    block3 = Conv2D(128,(3,3),activation="relu", kernel_initializer="he_uniform", padding="same",input_shape=input_shape)(block3)
+    block3 = MaxPooling2D(2, 2)(block3)
+    block3 = Dropout(0.2)(block3)
+    ## flatten layer ##
+    flatten = Flatten()(block3)
+    ## last layer ##
+    last_ly = Dense(128,activation="relu", kernel_initializer="he_uniform")(flatten)
+    last_ly = Dropout(0.2)(last_ly)
+    last_ly = Dense(64, activation="relu", kernel_initializer="he_uniform")(last_ly)
+    last_ly = Dropout(0.2)(last_ly)
+    last_ly = Dense(32, activation="relu", kernel_initializer="he_uniform")(last_ly)
+    last_ly = Dropout(0.2)(last_ly)
+    output1 = Dense(10, activation="softmax", kernel_initializer="he_uniform")(last_ly)
+    model = Model(input1,output1)
+    # compile model
+    model = model.compile(loss='categorical_crossentropy',
+                                      optimizer='adam',
+                                      metrics='accuracy')
+    return model
+
+def summerize_model(history):
+    # plot loss
+    plt.subplot(211)
+    plt.title('Cross Entropy Loss')
+    plt.plot(history.history['loss'], color='blue', label='train')
+    plt.plot(history.history['val_loss'], color='orange', label='test')
+    # plot accuracy
+    plt.subplot(212)
+    plt.title('Classification Accuracy')
+    plt.plot(history.history['accuracy'], color='blue', label='train')
+    plt.plot(history.history['val_accuracy'], color='orange', label='test')
+    # save plot to file
+    filename = sys.argv[0].split('/')[-1]
+    plt.savefig(filename + '_plot.png')
+    plt.close()
 
 
 
 if __name__ == "__main__":
     X_train, y_train, X_test, y_test = load_dataset()
+    # get model
+    model = CNN_model()
+    # fit model
+    history = model.fit(x=X_train,
+                        y=y_train,
+                        batch_size=32,
+                        epochs=500,
+                        validation_split=0.25,
+                        validation_data=(X_test,y_test),
+                        verbose=False)
+    _, acc = model.evaluate(X_test,y_test, verbose=False)
+    print(">%.3f"%(acc*100))
+    summerize_model(history)
     print("finish")
